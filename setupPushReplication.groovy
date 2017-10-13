29e5e7274151840a2483f90580aba7807f2d3a38
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 22:55:40 +0000

    File changed by admin

--- setupPushReplication.groovy
+++ setupPushReplication.groovy
@@ -1,12 +1,12 @@
 artifactoryToUserInput = userInput (
-    name : "Input to name artifactory",
+    simpleName : "Input to name artifactory",
     type : "ARTIFACTORY",
     description : "please provide a to name artifactory"
 )
 
-artifactory(artifactoryToUserInput.name) {
+artifactory(artifactoryToUserInput.simpleName) {
   localRepository("local-repository-key") {
-    replication(artifactoryToUserInput.name) {
+    replication(artifactoryToUserInput.simpleName) {
       username "admin"
       password "password"
 	  url http://localhost:8082/artifactory/bower-local
