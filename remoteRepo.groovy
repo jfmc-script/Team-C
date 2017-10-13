8de770638c35c397125e60598c8ccf12998506d8
Guru Ramanathaiah <gurur@jfrog.com>
2017-10-13 16:43:46 -0700

    Adding remote repo

---
+++ remoteRepo.groovy
@@ -0,0 +1,14 @@
+repository('remote-repository-key') {
+    description "Public description"
+    notes "Some internal notes"
+    includesPattern "**/*" // default
+    excludesPattern "" // default
+    repoLayoutRef "maven-2-default"
+    propertySets // (["ps1", "ps2"])
+    archiveBrowsingEnabled false
+    blackedOut false // default
+    url "http://host:port/some-repo"
+    username "remote-repo-user"
+    password "pass"
+    proxy "proxy-ref"
+  }
