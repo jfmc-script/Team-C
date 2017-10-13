74d9d5a46436c220663ae4afb39c72fd724415be
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 20:48:54 +0000

    File changed by admin

--- createLocalRepo.groovy
+++ createLocalRepo.groovy
@@ -1,23 +1,13 @@
-repokey = userInput (
-    type : "STRING", // "BOOLEAN", "INTEGER", "INSTANCE", "REPOSITORY"
-    description : "Repository Key",
-    validations : (["cron"])
-  )
-
-localRepository(repokey) {
-  description "Public Description"
-  notes "Some internal notes"
-  includesPattern "**/*" // default
-  excludesPattern "" // default
-  repoLayoutRef "maven-2-default"
-  packageType "generic" // "maven" | "gradle" | "ivy" | "sbt" | "nuget" | "gems" | "npm" | "bower" | "debian" | "pypi" | "docker" | "vagrant" | "gitlfs" | "yum" | "generic" 
-  debianTrivialLayout false
-  checksumPolicyType "client-checksums" // default | "server-generated-checksums"
-  handleReleases true // default
-  handleSnapshots true // default
-  maxUniqueSnapshots  0 // default
-  snapshotVersionBehavior "unique" // "non-unique" default | "deployer"
-  blackedOut false // default
-  archiveBrowsingEnabled true
-
+artifactory('art1'){
+localRepository('aaa') {
+    description "Public description"
+    notes "Some internal notes"
+    includesPattern "**/*" // default
+    excludesPattern "" // default
+    repoLayoutRef "maven-2-default"
+    propertySets // (["ps1", "ps2"])
+    archiveBrowsingEnabled false
+    blackedOut false // default
+    packageType "generic"
+  }
 }
