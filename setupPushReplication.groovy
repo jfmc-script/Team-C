5112123c0ca8319a1209dc61fe26477f8d697b3f
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 22:32:56 +0000

    File changed by admin

--- setupPushReplication.groovy
+++ setupPushReplication.groovy
@@ -9,7 +9,7 @@ artifactory(artifactoryToUserInput.name) {
     replication(artifactoryToUserInput) {
       username admin
       password password
-	  url https://artifactory-edge2-team-c.jfrogdev.co/artifactory
+	  url https://artifactory-edge2-team-c.jfrogdev.co/artifactory/
       cronExp "0 0/9 14 * * ?"
       socketTimeoutMillis 15000
     }
