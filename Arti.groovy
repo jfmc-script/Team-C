d7712311919d5f46890255b21e8d3640282f947a
jfmc-script <32617524+jfmc-script@users.noreply.github.com>
2017-10-13 15:19:20 +0300

    Update Arti.groovy

--- Arti.groovy
+++ Arti.groovy
@@ -1,17 +1,6 @@
-artifactoryToUserInput = userInput (
-    name : "Input to name artifactory",
-    type : "ARTIFACTORY",
-    description : "please provide a to name artifactory"
-)
-
-artifactory(artifactoryToUserInput.name) {
-  localRepository("local-repository-key") {
-    replication(artifactoryToUserInput) {
-      username artifactoryToUserInput.credentials.userName
-      password artifactoryToUserInput.credentials.password
-	  url http://localhost:8082/artifactory/bower-local
-      cronExp "0 0/9 14 * * ?"
-      socketTimeoutMillis 15000
-    }
-  }
+artifactory("Art-1") {
+localRepository("docker-local-a") {
+  packageType "docker"
+  description "My local Docker registry-vvvvvv"
+}
 }
