cdf455e1539171744e5273b466d241d814806d23
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 23:21:29 +0000

    File changed by admin

--- createLocalRepo.groovy
+++ createLocalRepo.groovy
@@ -32,3 +32,17 @@ description "Public description"
     packageType "generic"
 }
 }
+
+artifactory("Art3-TEAMD") { //Enter the target/s artifactory. Repeat the artifactory closure for every target in star topology.
+localRepository(repokeyArt1) {
+description "Public description"
+    notes "Some internal notes"
+    includesPattern "**/*" // default
+    excludesPattern "" // default
+    repoLayoutRef "maven-2-default"
+    propertySets // (["ps1", "ps2"])
+    archiveBrowsingEnabled false
+    blackedOut false // default
+    packageType "generic"
+}
+}
