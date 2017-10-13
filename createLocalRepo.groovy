7f58a8bfb9579c1b4b539f47de733818eee5ea9e
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 20:56:23 +0000

    File changed by admin

--- createLocalRepo.groovy
+++ createLocalRepo.groovy
@@ -17,3 +17,18 @@ description "Public description"
     packageType "generic"
 }
 }
+
+
+artifactory("Art-2") {
+localRepository(repokeyArt1) {
+description "Public description"
+    notes "Some internal notes"
+    includesPattern "**/*" // default
+    excludesPattern "" // default
+    repoLayoutRef "maven-2-default"
+    propertySets // (["ps1", "ps2"])
+    archiveBrowsingEnabled false
+    blackedOut false // default
+    packageType "generic"
+}
+}
