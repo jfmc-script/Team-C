02ff74bbfabf598064de65c3eb2debd39277c499
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 21:29:48 +0000

    File changed by admin

---
+++ setupPushReplication.groovy
@@ -0,0 +1,17 @@
+artifactoryToUserInput = userInput (
+    name : "Input to name artifactory",
+    type : "ARTIFACTORY",
+    description : "please provide a to name artifactory"
+)
+
+artifactory(artifactoryToUserInput.name) {
+  localRepository("teamc-generic-local") {
+    replication(artifactoryToUserInput) {
+      username admin
+      password password
+	  url https://artifactory-edge2-team-c.jfrogdev.co/artifactory
+      cronExp "0 0/9 14 * * ?"
+      socketTimeoutMillis 15000
+    }
+  }
+}
