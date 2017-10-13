f61a17c1524776db4eb262e07830f9c8cab01708
mission-control <mission-control@mission-control-1426300637-zp08j>
2017-10-13 20:50:00 +0000

    File changed by admin

--- createLocalRepo.groovy
+++ createLocalRepo.groovy
@@ -1,13 +1,6 @@
-artifactory('art1'){
-localRepository('aaa') {
-    description "Public description"
-    notes "Some internal notes"
-    includesPattern "**/*" // default
-    excludesPattern "" // default
-    repoLayoutRef "maven-2-default"
-    propertySets // (["ps1", "ps2"])
-    archiveBrowsingEnabled false
-    blackedOut false // default
-    packageType "generic"
-  }
+artifactory("Art-1") {
+localRepository("docker-local-a") {
+  packageType "docker"
+  description "My local Docker registry-vvvvvv"
+}
 }
